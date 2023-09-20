b45da63e215bedcdeb8ab006b40731ed8ab580e9
oys2002 <yaminisaraswathi.oggu@gmail.com>
2023-09-20 22:06:41 +0530

    basic

--- python.py
+++ python.py
@@ -1,5 +1,34 @@
-x=input("Enter any number:")
-if x%2==0:
-    print(x,"is a even no.")
-else:
-    print(x,"is a odd no.")
+from flask import Flask, request, render_template, redirect, url_for
+
+
+app = Flask(__name__)
+
+
+users = {
+    'user1': {'password': 'password1', 'email': 'user1@example.com'},
+    'user2': {'password': 'password2', 'email': 'user2@example.com'}
+}
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if username in users and users[username]['password'] == password:
+        email = users[username]['email']
+        return f'Logged in as {username} (Email: {email})'
+    else:
+        return 'Invalid credentials. Please try again.'
+
+
+@app.route("/api", methods = ['GET'])
+def api1():
+    return render_template("show.html", ins="Checking Sample API")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
